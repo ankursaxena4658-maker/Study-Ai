f46a55a3e7ecb4174d4758f0e6c913132d0a2ebc
ankursaxena4658-maker <ankursaxena4658@gmail.com>
2026-03-11 07:25:22 +0530

    Initial project upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'studyai-v3';
+const STATIC = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC).catch(() => {})));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
